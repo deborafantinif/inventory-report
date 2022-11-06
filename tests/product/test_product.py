c93c754470c182dc835c3afa4022c252a508e1a0
deborafantinif <deboraferfan@gmail.com>
2022-11-06 15:27:00 -0300

    TEST: criação de produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "calendar",
+        "register LTDA",
+        "12-02-2002",
+        "29-12-2999",
+        "0002456",
+        "local seco, sem expor ao sol",
+    )
+
+    response_product = """O produto calendar
+                        fabricado em 12-02-2002
+                        por register LTDA com validade
+                        até 29-12-2999
+                        precisa ser armazenado local seco, sem expor ao sol."""
+
+    assert product.__repr__() == response_product
+    assert product.nome_do_produto == "calendar"
+    assert product.nome_da_empresa == "register LTDA"
+    assert product.data_de_fabricacao == "12-02-2002"
+    assert product.data_de_validade == "29-12-2999"
+    assert product.numero_de_serie == "0002456"
+    assert (
+        product.instrucoes_de_armazenamento == "local seco, sem expor ao sol"
+    )
