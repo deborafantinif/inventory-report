e4b04f1d32108063a1c81308bb83627814d3c823
deborafantinif <deboraferfan@gmail.com>
2022-11-06 17:08:28 -0300

    FEAT: relatório simples dos produtos

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from datetime import date
+
+
+class SimpleReport:
+    @staticmethod
+    def generate(products):
+        manufacturing_date = [
+            product["data_de_fabricacao"] for product in products
+        ]
+        today = date.fromisoformat(date.today().isoformat())
+        validate_date = [
+            product["data_de_validade"] for product in products
+            if date.fromisoformat(product["data_de_validade"]) > today
+        ]
+        company_names = [product["nome_da_empresa"] for product in products]
+        oldest_product = min(manufacturing_date)
+        newest_product = min(validate_date)
+        company_frequency = Counter(company_names)
+        more_repeated_company = [
+            product
+            for product in company_frequency.keys()
+            if company_frequency[product] == max(company_frequency.values())
+        ]
+        return (
+            f"Data de fabricação mais antiga: {oldest_product}\n"
+            f"Data de validade mais próxima: {newest_product}\n"
+            f"Empresa com mais produtos: {more_repeated_company[0]}\n"
+        )
