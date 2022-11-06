5e615a35e6eb01b948f1158ad3508b865ea73ea0
deborafantinif <deboraferfan@gmail.com>
2022-11-06 19:32:21 -0300

    FEAT: manipulação arquivo XML

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,13 @@
 import csv
 import json
-
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    @classmethod
-    def import_data(cls, path, type_report):
+    @staticmethod
+    def handle_read_file(path):
         with open(path, "r") as file_products:
             products = ""
             if "csv" in path:
@@ -16,7 +16,16 @@ class Inventory:
                 )
             elif "json" in path:
                 products = json.load(file_products)
-            if type_report == "simples":
-                return SimpleReport.generate(products)
-            if type_report == "completo":
-                return CompleteReport.generate(products)
+            elif "xml" in path:
+                products = xmltodict.parse(file_products.read())["dataset"][
+                    "record"
+                ]
+            return products
+
+    @classmethod
+    def import_data(cls, path, type_report):
+        products = cls.handle_read_file(path)
+        if type_report == "simples":
+            return SimpleReport.generate(products)
+        if type_report == "completo":
+            return CompleteReport.generate(products)
