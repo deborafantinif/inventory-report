27b10930268c0a0385005e4c40e8fd963a46f75e
deborafantinif <deboraferfan@gmail.com>
2022-11-06 19:16:56 -0300

    FEAT: manipulação de arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -6,10 +8,14 @@ from inventory_report.reports.simple_report import SimpleReport
 class Inventory:
     @classmethod
     def import_data(cls, path, type_report):
-        with open(path, 'r') as file_products:
-            products = list(csv.DictReader(
-                file_products, delimiter=",", quotechar='"'
-            ))
+        with open(path, "r") as file_products:
+            products = ""
+            if "csv" in path:
+                products = list(
+                    csv.DictReader(file_products, delimiter=",", quotechar='"')
+                )
+            elif "json" in path:
+                products = json.load(file_products)
             if type_report == "simples":
                 return SimpleReport.generate(products)
             if type_report == "completo":
